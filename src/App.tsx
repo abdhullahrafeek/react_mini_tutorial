18796932b6d0772ae4b208bef56ffab58d4c9131
Abdhullah Rafeek <rafeekabdhullah13.work@gmail.com>
2026-02-02 10:38:57 +0800

    Modified codes for props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App(){
 
   let items = ["Hatton", "Kandy", "Colombo", "Maskeliya", "Kaduwela"];
 
-  return <div><ListGroup items={items} heading="Cities"/></div>;
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
+
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
 }
 
 export default App;
